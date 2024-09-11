18c819a9faafc58aca88d1d1fc760f90d1271226
Farothtc <mvkirimi@gmail.com>
2024-09-11 13:22:57 +0300

    General page structure added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,21 @@
-export function App() {
-  return <></>;
+import { About } from "./components/About.jsx";
+import { Photo } from "./components/Photo.jsx";
+import { Name } from "./components/Name.jsx";
+import { Buttons } from "./components/Buttons.jsx";
+
+export default function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="card">
+          <Photo />
+          <div className="card-body">
+            <Name />
+            <Buttons />
+            <About />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
