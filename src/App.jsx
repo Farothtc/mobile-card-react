da0115b1618c8eaa53e3926f979e60cf0880ad88
Farothtc <mvkirimi@gmail.com>
2024-09-11 17:00:44 +0300

    Footer component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import { About } from "./components/About.jsx";
 import { Photo } from "./components/Photo.jsx";
 import { Name } from "./components/Name.jsx";
 import { Buttons } from "./components/Buttons.jsx";
+import { Footer } from "./components/Footer.jsx";
 
 export default function App() {
   return (
     <div className="container">
       <div className="row">
-        <div className="card">
+        <div className="card border-0 mt-2">
           <Photo />
           <div className="card-body">
             <Name />
             <Buttons />
             <About />
           </div>
+          <Footer />
         </div>
       </div>
     </div>
